37ceb320d9f1c5198836a7047533e092d2ab9c71
sam <samcesario1@gmail.com>
2017-06-07 17:30:12 -0400

    Fib working

--- print_fibonacci.c
+++ print_fibonacci.c
@@ -1,7 +1,7 @@
 
 /**
 	Class to handle the printing of Fibonacci's sequence
-	Description: Program to generate the first n Fibonacii numbers F(n)
+	Description: Program to generate the first n Fibonacci numbers F(n)
 	printing:
 		1. "Buzz" w/ F(n) is divisible by 3.
 		2. "Fizz" w/ F(n) is divisible by 5.
@@ -14,9 +14,37 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int print_fib(int n){
+
+	printf("Fibonacci Sequence Printing...\n");
+	//auto variables
+	int first=0, second=1, sum=0;
+
+	printf("%i ", first);
+	printf("%i ", second);
+
+	for(int i=0; i<n;i++){
+		first++;
+		second++;
+		sum = first + second;
+		printf(" %i",sum);
+	}
+	printf("\n");
+	return 1;
+}
 
 
 int main(int argc, char *argv[]){
+
+	if(argv[0] == NULL){
+		printf("Please provide one arguement\n");
+	}
+	else{
+		// Convert String to long
+		long input_n = strtol(argv[1], NULL, 10);
+		print_fib(input_n);
+	}
 
 }
