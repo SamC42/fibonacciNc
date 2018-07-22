bd6d5a65c48f11e0a69bf3a207c2234c2a6fefd8
sam <samcesario1@gmail.com>
2018-07-22 18:09:44 -0400

    added some more pointer examples

--- practice_c/practice.c
+++ practice_c/practice.c
@@ -10,17 +10,37 @@ int is just a larger character type
 */
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 // 5/12/2017
+// Declaring Pointers
 void testPointers(void){
+	
+	
+	//Example 1 - Copies address of testVal into p1 so that you can get to the value via *p1 
 	int i = 13;
 	printf(" I: %i (value)\n",i );
-	int *j = &i; // j is a declaration, so a pointer
+	printf(" &I: %i (address)\n",&i );
+	int *j = &i; // j is a declaration, so a pointer, address of i stored in j, value of i stored in *j
+	//int j = &i; // Returns an error since j is an int and &i is an address (pointer)
 	printf("*j: %i (value)\n",*j ); //should print out the value of i (star indicates value of pointer)
-	//printf(" j: %i (address)\n",j ); //should print out the address of i  
-	int *k = j;  // k is a declaration, so a pointer
-	printf("*k: %i (value)\n",*k ); // should print out value of k which is j which is 13
+	printf(" j: %i (address)\n",j ); //should print out the address of i
+
+	int *k = j;  // k is a declaration, so a pointer, copies address of j into a pointer k
+	printf("*k: %i (value)\n",*k ); // should print out value of *k which is j which is 13
 	*j = 12;     // j is not a declaration so a value (i.e. a value can be assigned)
 	printf("*j: %i (value)\n", *j); // should print out value since not in declaration
+	
+	int firstvalue, secondvalue;
+	int * mypointer;
+
+	mypointer = &firstvalue; // Sets mypointer to the address firstValue, now by manipulating mypointer we can change firstValue's value
+	printf("%i\n",mypointer);
+	*mypointer = 10; // Sets the value of mypointer to 10 (my pointer points to firstValue so in effect we are changing firstvalue)
+	mypointer = &secondvalue; // We can now re use mypointer and set it to another address
+	*mypointer = 20;
+	std::cout << "firstvalue is " << firstvalue << '\n';
+	std::cout << "secondvalue is " << secondvalue << '\n';
 }
 
 // Test Arrays, Strings and Pointers
@@ -104,11 +124,12 @@ int main(int argc, char *argv[]){
 	//printf("Argv[0]: %s\n",argv[0] ); // Prints out the value of argv, so the full string
 	//printf("Argv[0] (with *): %c\n",*argv[0]); // Argv is array of char string, so ptr argv is first char 
 
-	//testPointers();
+	testPointers();
+	//morePointers();
 	//testASP();
 	//write2File();
 	//readFile(argv[1]);
 	//dataTypes();
-	gnpChar();
+	//gnpChar();
 }
 
